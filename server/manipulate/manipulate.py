24726b8abf36b5a7aaa23e11831f39f0cd451b56
Edward Yang <jh.edwardyang@gmail.com>
2015-09-19 15:27:42 -0400

    Demo WAV

--- server/manipulate/manipulate.py
+++ server/manipulate/manipulate.py
@@ -1,9 +1,27 @@
-import argparse
-from pydub import AudioSegment
+# Pydub from https://github.com/jiaaro/pydub#installation
 
-def pydub(filename):
+# USAGE
+# manipulate(filename, output), returns nothing, saves to file output
+#   filename: .wav
+#   output: output .wav
+# OR
+# python manipulate.py --filename <.wav> --output <.wav>
+
+
+from pydub import AudioSegment
+import argparse
+
+def manipulate(filename, output):
 	song = AudioSegment.from_wav(filename)
 	# song = AudioSegment.from_mp3("never_gonna_give_you_up.mp3")
+
+	animals = AudioSegment.from_wav("../animals.wav")
+	beat = AudioSegment.from_wav("../bg128bmp.wav")
+	bass = beat.append(beat, crossfade=100).append(beat, crossfade=100)
+	music = animals.overlay(bass, position=620)
+	music.export("woot.wav", format="wav")
+
+
 
 	# pydub does things in milliseconds
 	ten_seconds = 10 * 1000
@@ -30,14 +48,16 @@ def pydub(filename):
 	# 2 sec fade in, 3 sec fade out
 	awesome = do_it_over.fade_in(2000).fade_out(3000)
 
-	awesome.export("mashup.wav", format="wav")
+	awesome.export(output, format="wav")
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Process .wav file to do fun stuff.')
     parser.add_argument('--filename', required=True,
                    help='.wav file for processing')
+    parser.add_argument('--output', required=True,
+    				help='.wav file for output')
 
     args = parser.parse_args()
 
-    pydub(args.filename);
+    manipulate(args.filename, args.output);
