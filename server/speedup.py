9d45ac83c9d32c84441a3c9db5a995fc8e2559ca
Edward Yang <jh.edwardyang@gmail.com>
2015-09-20 01:23:23 -0400

    remove non128

--- server/speedup.py
+++ server/speedup.py
@@ -8,18 +8,18 @@ import os.path
 def ver3(filename):
     oldbpm,peaks = bpm_detection.bpm_detection(filename,3)
     oldsong = AudioSegment.from_wav(filename)
-    if (oldbpm < 140):
-        rate1 = 140/oldbpm
-        song140 = oldsong.speedup(rate1)
-        song140.export(os.path.splitext(args.filename)[0] + "-140.wav", format="wav")
+    # if (oldbpm < 140):
+    #     rate1 = 140/oldbpm
+    #     song140 = oldsong.speedup(rate1)
+    #     song140.export(os.path.splitext(args.filename)[0] + "-140.wav", format="wav")
     if (oldbpm < 128):
         rate2 = 128/oldbpm
         song128 = oldsong.speedup(rate2)
         song128.export(os.path.splitext(args.filename)[0] + "-128.wav", format="wav")
-    if (oldbpm < 86):
-        rate3 = 86/oldbpm
-        song86 = oldsong.speedup(rate3)
-        song86.export(os.path.splitext(args.filename)[0] + "-86.wav", format="wav")
+    # if (oldbpm < 86):
+    #     rate3 = 86/oldbpm
+    #     song86 = oldsong.speedup(rate3)
+    #     song86.export(os.path.splitext(args.filename)[0] + "-86.wav", format="wav")
     return
 
 if __name__ == '__main__':
